a4b01d909be631747c93386f076c3b28db684049
loki00345 <99328513+loki00345@users.noreply.github.com>
2024-05-31 23:35:20 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*ex1*/
+    /*cout << "The grass is grean," << endl;
+    cout << "The sky is blue," << endl;
+    cout << "The moon is white," << endl;
+    cout << "The clouds are, too." << endl;
+    cout << "\t\tnoname";*/
+
+    /*ex2*/
+    /*cout << "Every" << endl;
+    cout << "hunter" << endl;
+    cout << "wants" << endl;
+    cout << "to" << endl;
+    cout << "know" << endl;
+    cout << "where" << endl;
+    cout << "does" << endl;
+    cout << "pheasant" << endl;
+    cout << "sit";*/
+
+    /*ex3*/
+    /*cout << "\tsell telephone." << endl;
+    cout << "\tsell house\b";
+    cout <<"\n\t..."<<endl;
+    cout << "\ttel.: 1234-567-8910" << endl;
+    cout << "\t1234\t";
+    cout << "|1234|"<<endl;
+    cout << "\t567\t";
+    cout << "|567|" << endl;
+    cout << "\t8910\t";
+    cout << "|8910|" << endl;*/
+
+
+
+}
+
